de67b0e193ba0f280503567adbc8c783dd02bbef
gitaufaith <faithgitz1@gmail.com>
2023-02-15 20:44:36 +0300

    master

--- Week-2/arithmetic_progression.py
+++ Week-2/arithmetic_progression.py
@@ -1,6 +1,11 @@
 #write a program that calculates arithmetic progression from 1 to 20
-n=20
-a =input("Enter the first term:")
-d=input("Enter the difference:")
-arithmeticprogression=(int(a) + (n-1)*int(d))
-print(arithmeticprogression)
+#formula n=nth term in the sequence,a=first term,d=common difference
+#AP=a+(n-1)d
+nth_term=20
+first_term=1
+common_difference=input("what is the common difference:(should be<9>)")
+if (int(common_difference)> 9):
+    print("out of range!!!")
+else:
+    arithmetic_progression=first_term +((nth_term -1) +int(common_difference))
+    print("The arithmetic progression is:",arithmetic_progression)
