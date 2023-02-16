51e1dd57c0e8bb3df3a2f69aea3ecb076a4e012d
gitaufaith <faithgitz1@gmail.com>
2023-02-16 19:04:15 +0300

    master

---
+++ Week-2/mean.py
@@ -0,0 +1,5 @@
+numbers=[1,2,3,4,5,6,7,8,9,10]
+sum_num =0
+for number in numbers :
+    avg_num=sum_num/10
+    print("the average of numbers:",avg_num)
---
+++ Week-2/numbers.py
@@ -0,0 +1,15 @@
+ #find the sum
+#average of list
+numbers= [4,5,1,2,9,7,10,8,3,6,0]
+sum_num=0
+for i in numbers:
+        sum_num +=i
+
+        avg=sum_num/len(numbers)
+
+        print("sum=" ,sum_num)
+        print("average=" ,avg)
+
+
+        
+    
