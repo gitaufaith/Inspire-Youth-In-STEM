756a189cdcfd12474fa54df3ca0b65f032248570
gitaufaith <faithgitz1@gmail.com>
2023-03-24 11:58:57 +0300

    initial commit

---
+++ Week-5/website/JavaScript/index.js
@@ -0,0 +1,21 @@
+let age =0
+let msg=""
+
+function add_numbers(num1+ number2)
+{
+    return num1+num2
+}
+add_numbers(23,34)
+console.log("Javascript console testing")
+document.getElementById("confirm_btn").click= check_age(){
+    age=document.getElementById("age").innerHTML.valueOf()
+if(age <=18){
+    msg="you are not allowed" 
+    document.getElementById("get_in").innerHTML=msg
+
+}else{
+    msg="welcome to the club" 
+    document.getElementById("get_in").innerHTML=msg
+     
+}
+}
